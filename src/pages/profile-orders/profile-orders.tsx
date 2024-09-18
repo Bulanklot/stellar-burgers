4db922835a2af3984b1812e758281f573c7c1733
Bulat Zinatullin <Zinatullinbn@gmail.com>
2024-09-18 16:14:07 +0300

    bulat

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,6 +1,6 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC, useEffect } from 'react';
+import { FC, useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from '../../services/store';
 import { allOrders, getOrders } from '../../slices/orders';
 
@@ -8,9 +8,12 @@ export const ProfileOrders: FC = () => {
   /** TODO: взять переменную из стора */
 
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getOrders);
+  const handleGetOrders = useCallback(() => {
+    dispatch(getOrders());
   }, [dispatch]);
+  useEffect(() => {
+    handleGetOrders();
+  }, [handleGetOrders]);
   const orders: TOrder[] = useSelector(allOrders);
 
   return <ProfileOrdersUI orders={orders} />;
